261bc43d473566eba1aed6ebe6c4f1d84d1d3587
Varshith Solipuram <vsolipuram5@gmail.com>
2019-01-01 23:53:32 -0500

    Update index.html

---
+++ js/main.js
@@ -0,0 +1,173 @@
+$(document).ready(function() {
+
+
+    /* Scroll hire me button to contact page */
+    $('.hire-me').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
+    });
+
+    /* For Bootstrap current state on portfolio sorting */
+
+    $('ul.nav-pills li a').click(function(e) {
+        $('ul.nav-pills li.active').removeClass('active')
+        $(this).parent('li').addClass('active')
+    })
+
+    /* portfolio mixitup */
+
+    $(window).load(function() {
+        var $container = $('.grid-wrapper');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        $('.grid-controls li a').click(function() {
+            $('.grid-controls .current').removeClass('current');
+            $(this).addClass('current');
+
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+    });
+
+
+    /* Magnific Popup */
+    /*$('.grid-wrapper').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+    /* Sticky menu */
+    $(".navbar").sticky({
+        topSpacing: 0
+    });
+
+
+    /* Scroll spy and scroll filter */
+    $('#main-menu').onePageNav({
+        currentClass: "active",
+        changeHash: false,
+        scrollThreshold: 0.5,
+        scrollSpeed: 750,
+        filter: "",
+        easing: "swing"
+    });
+
+    /* Charts*/
+
+    $('.chart').waypoint(function() {
+        $(this).easyPieChart({
+            barColor: '#3498db',
+            size: '150',
+            easing: 'easeOutBounce',
+            onStep: function(from, to, percent) {
+                $(this.el).find('.percent').text(Math.round(percent));
+            }
+        });
+    }, {
+        triggerOnce: true,
+        offset: 'bottom-in-view'
+    });
+
+
+    /* VEGAS Home Slider */
+	
+	    $('#page-welcome').vegas({
+        slides: [
+            { src: 'img/slider/01.jpg' },
+            { src: 'img/slider/02.jpg' },
+            { src: 'img/slider/03.jpg' },
+			{ src: 'img/slider/04.jpg' }
+        ],
+    overlay: true
+});
+
+    $("#vegas-next").click(function() {
+        $('#page-welcome').vegas('next');
+    });
+    $("#vegas-prev").click(function() {
+       $('#page-welcome').vegas('previous');
+    });
+
+    $(document).ready(function ($) {
+
+        // delegate calls to data-toggle="lightbox"
+        $(document).delegate('*[data-toggle="lightbox"]:not([data-gallery="navigateTo"])', 'click', function(event) {
+            event.preventDefault();
+            return $(this).ekkoLightbox({
+                onShown: function() {
+                    if (window.console) {
+                        return console.log('onShown event fired');
+                    }
+                },
+                onContentLoaded: function() {
+                    if (window.console) {
+                        return console.log('onContentLoaded event fired');
+                    }
+                },
+                onNavigate: function(direction, itemIndex) {
+                    if (window.console) {
+                        return console.log('Navigating '+direction+'. Current item: '+itemIndex);
+                    }
+                }
+            });
+        });
+
+        //Programatically call
+        $('#open-image').click(function (e) {
+            e.preventDefault();
+            $(this).ekkoLightbox();
+        });
+        $('#open-youtube').click(function (e) {
+            e.preventDefault();
+            $(this).ekkoLightbox();
+        });
+
+        $(document).delegate('*[data-gallery="navigateTo"]', 'click', function(event) {
+            event.preventDefault();
+            return $(this).ekkoLightbox({
+                onShown: function() {
+                    var lb = this;
+                    $(lb.modal_content).on('click', '.modal-footer a#jumpit', function(e) {
+                        e.preventDefault();
+                        lb.navigateTo(2);
+                    });
+                    $(lb.modal_content).on('click', '.modal-footer a#closeit', function(e) {
+                        e.preventDefault();
+                        lb.close();
+                    });
+                }
+            });
+        });
+
+    });
+
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-43208246-2', 'ashleydw.github.io');
+      ga('send', 'pageview'); 
+
+});
